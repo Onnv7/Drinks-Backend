d0b29868f92d8ebbeff43a743230fcf9c415fb88
Nguyen Van An <nva6112002@gmail.com>
2023-09-24 19:12:27 +0700

    Create endpoint CRUD for category

--- src/main/java/com/hcmute/drink/collection/ProductCollection.java
+++ src/main/java/com/hcmute/drink/collection/ProductCollection.java
@@ -7,6 +7,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
@@ -25,5 +26,6 @@ public class ProductCollection {
     private List<String> size;
     private String description;
     private List<ToppingModel> toppingList;
-    private String categoryId;
+    @DBRef
+    private CategoryCollection category;
 }
--- src/main/java/com/hcmute/drink/utils/CloudinaryUtils.java
+++ src/main/java/com/hcmute/drink/utils/CloudinaryUtils.java
@@ -1,6 +1,7 @@
 package com.hcmute.drink.utils;
 
 import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import com.hcmute.drink.constant.CloudinaryConstant;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
@@ -28,15 +30,21 @@ public class CloudinaryUtils {
                 .get(URL_PROPERTY)
                 .toString();
     }
-    public String uploadFileToFolder(String pathName, String fileName, MultipartFile multipartFile) throws IOException {
-        return cloudinary.uploader()
+    public HashMap<String, String> uploadFileToFolder(String pathName, String fileName, MultipartFile multipartFile) throws IOException {
+        var file = cloudinary.uploader()
                 .upload(multipartFile.getBytes(),
                         Map.of(
                                 PUBLIC_ID, fileName,
                                 UPLOAD_PRESET, pathName,
                                 OVERWRITE, true
-                        ))
-                .get(URL_PROPERTY)
-                .toString();
+                        ));
+
+        return (HashMap<String, String>) file;
+    }
+
+    public void deleteImage(String publicId) throws IOException {
+        // Xóa hình ảnh từ Cloudinary bằng cách sử dụng public ID
+        Map<String, String> options = ObjectUtils.asMap("invalidate", true);
+        cloudinary.uploader().destroy(publicId, options);
     }
 }
